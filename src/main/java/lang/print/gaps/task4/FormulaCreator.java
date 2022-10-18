4b66eef10828cc787eb004fe35f9dd8cf5d48104
Maryna Kidun <marykidun@gmail.com>
2022-10-18 23:30:20 +0300

    Finished 3 tasks with Primitieves

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        double result = ((9 * Math.pow(a, 2) - (5 * b) + 2 + a - 7)) * ((a + b - 4 * a * b) / 2);
+        System.out.println(result);
     }
 }
+
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        c = 99;
+        char h = 'h';
+        h = 104;
+        char a = 'a';
+        a = 97;
+        char r = 'r';
+        r = 114;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
     }
 }
+
